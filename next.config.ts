f1adc04c194bc2c8a7669522d3664582a7a71c39
Arifin N.H <arifinnh311@gmail.com>
2026-08-29 19:12:49 +0700

    chore: initialize Next.js 16 project with Tailwind CSS v4 and TypeScript

---
+++ next.config.ts
@@ -0,0 +1,23 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "50mb",
+    },
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+      {
+        protocol: "http",
+        hostname: "**",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
